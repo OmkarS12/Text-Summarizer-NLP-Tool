16a93098f8c5890b062bd4bf695f92a8d9549727
OmkarS12 <sadekar.o@northeastern.edu>
2024-05-09 20:19:13 -0400

    data ingestion pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
+
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
